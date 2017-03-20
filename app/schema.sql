d2b5495d44f440dee9c1fef80ebcc09c2bc1a1dd
Paul Goodman <paulgoodman@Pauls-MacBook-Pro.local>
2017-03-20 18:33:38 -0400

    First commit, not functional yet, working on loading initial data.

---
+++ app/schema.sql
@@ -0,0 +1,14 @@
+DROP TABLE IF EXISTS fics;
+CREATE TABLE fics (
+    id integer primary key autoincrement,
+    title text not null,
+    url text not null,
+    synopsis text not null,
+    rating integer not null,
+    comments text not null,
+    rating integer not null,
+    author text not null,
+    complete text not null,
+    mood text not null,
+    tvtropes text
+);
