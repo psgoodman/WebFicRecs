9044d06126ba60367f1b6fff4eeb00d511221274
Paul Goodman <paulgoodman@Pauls-MacBook-Pro.local>
2017-04-04 15:24:49 -0400

    Cleaned up somewhat. itemTemplate is handy.

--- app/static/main.js
+++ app/static/main.js
@@ -17,21 +17,49 @@ $("#jsGrid").jsGrid({
     },
 
     fields: [
-        { name:"title", title: "Title", type: "text", width: 150 },
+        {
+            name:"title",
+            title: "Title",
+            type: "text",
+            width: 150,
+            itemTemplate: function(value, item) {
+                var html = '<a href="' + item.url + '">' + value + '</a>'
+                return html
+            }
+        },
         { name:"url", title: "URL", type: "text", visible: false, sorting: false },
         { name:"synopsis", title: "Synopsis", type: "text", width: 300, sorting: false },
-        { name:"rating", title: "Rating", type: "number", width: 50 },
+        {
+            name:"rating",
+            title: "Rating",
+            type: "number",
+            width: 70,
+            itemTemplate: function(value, item) {
+                var ratings = ["A+", "A", "A-","B+", "B", "B-","C+", "C", "C-"]
+                return ratings[value]
+            }
+        },
         { name:"comments", title: "Comments", type: "text", width: 300, sorting: false },
         {
             name: "length",
             title: "Length",
             type: "number",
-            width: 50
+            width: 70
         },
-        { name:"author", title: "Author", type: "text", width: 80 },
-        { name:"complete", title: "Complete", type: "text", width: 50 },
-        { name:"mood", title: "Mood", type: "text", width: 150 },
-        { name: "tvtropes", title: "TV Tropes", type: "text", width: 150,
-            sorting: false }
+        { name:"author", title: "Author", type: "text", width: 100 },
+        { name:"complete", title: "Complete", type: "text", width: 70 },
+        { name:"mood", title: "Mood", type: "text", width: 120 },
+        {
+            name: "tvtropes",
+            title: "TV Tropes",
+            type: "text",
+            width: 150,
+            sorting: false,
+            itemTemplate: function(value, item) {
+                if (value === "") {return ""}
+                var html = '<a href="' + value + '">' + item.title + '</a>'
+                return html
+            }
+        }
     ]
 });
